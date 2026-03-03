746151f9ab7e76df0c1fe7076307a55cdb68e74b
MuzhaHD <chatbot12021998@gmail.com>
2026-03-03 19:24:27 +1000

    folder tree finished

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main className="min-h-screen flex items-center justify-center bg-[#0B0F1A] text-white">
-      <h1 className="text-3xl font-bold text-[#F5C518]">
-        CHINDO Development Mode 🚀
-      </h1>
-    </main>
-  );
-}
